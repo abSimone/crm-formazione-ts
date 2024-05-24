e841f14e04e0d86901b80113a31c7a7b34cddbe3
mMatteo <matteomurino2@gmail.com>
2024-05-24 12:50:26 +0200

    Creata funzione per la card e importato script bundle.js in index3.html

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,26 @@ async function card() {
   let data = await response.json();
   console.log(data);
 
+  let labelDynamic: string = '';
+
+  data.forEach((element: any) => {
+    labelDynamic += `<label>${element.ruolo}: ${element.email}</label>`;
+  });
+
+  let card: HTMLElement = document.getElementById('card')!;
+  let elemementHtml: string = `
+    <div id="info" class="card-body">
+      <label class="d-block p-2">Aula</label>
+      ${labelDynamic}
+      <label class="d-block p-2"> Inizio: 15 -08 - 21 </label>
+      <label class="d-block p-2"> Fine: 25 - 11 - 21 </label>
+      <label class="d-block p-2"> Corso: Informatica </label>
+      <label class="d-block p-2"> Esterno </label>
+    </div>
+  `;
+  console.log(elemementHtml);
+
+  card.innerHTML = elemementHtml;
 }
 card();
 main();
