45508ccd931d3b17e07a4df3825694749d789d61
mMatteo <matteomurino2@gmail.com>
2024-05-24 12:11:20 +0200

    aggiunta funzione per creazione della card di index3.html

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,13 @@ function main() {
   corsiCard.addEventListener("click", () => window.location.assign(""));
 }
 
+const endopoint: string = "http://localhost:8000/";
+
+async function card() {
+  const response = await fetch(`${endopoint}users`);
+  let data = await response.json();
+  console.log(data);
+
+}
+card();
 main();
