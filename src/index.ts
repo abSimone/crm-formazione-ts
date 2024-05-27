261b0e02819ec5b53b7ad0a2630e212bff374d86
AlesDod02 <alessandro.dod@gmail.com>
2024-05-27 09:21:44 +0200

    working

--- src/index.ts
+++ src/index.ts
@@ -45,47 +45,27 @@ async function compilaTabella() {
   const response = await fetch(`${endopoint}courses`);
   let persone = await response.json();
   console.log(persone);
-  const tbody = document.querySelector('tbody')!;
-
-
-  persone.forEach((persona: any) => {
-    const row = document.createElement('tr');
-
-
-    const cognomeCell = document.createElement('td');
-    cognomeCell.textContent = persona.cognome;
-    row.appendChild(cognomeCell);
-
-    const nomeCell = document.createElement('td');
-    nomeCell.textContent = persona.nome;
-    row.appendChild(nomeCell);
-
-    const emailCell = document.createElement('td');
-    emailCell.textContent = persona.email;
-    row.appendChild(emailCell);
-
-    const etaCell = document.createElement('td');
-    etaCell.textContent = persona.eta;
-    row.appendChild(etaCell);
-
-    const sedeCell = document.createElement('td');
-    sedeCell.textContent = persona.sede;
-    row.appendChild(sedeCell);
-
-    const noteCell = document.createElement('td');
-    if (persona.note) {
-      const noteLink = document.createElement('a');
-      noteLink.href = '#';
-      noteLink.textContent = 'Note formatore';
-      noteCell.appendChild(noteLink);
-    }
-    row.appendChild(noteCell);
-
-
+  persone.forEach((element: any) => {
+    let table: HTMLElement = document.getElementById('table')!;
+    let elemementHtml: string = `
+    <td>Rossi</td>
+                        <td>${persone.cognome}</td>
+                        <td>${persone.nome}</td>
+                        <td>${persone.email}</td>
+                        <td>${persone.età}</td>
+                        <td>${persone.sede}</td>
+                        <td><a href="${persone.note}">Note formatore</a></td>
+                        <td>
+                            <div class="dropdown">
+                                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
+                                    data-bs-toggle="dropdown" aria-expanded="false">▼</button>
+                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                    <li><a class="dropdown-item" href="#">ritira</a></li>
+                                    <li><a class="dropdown-item" href="#">Visualliza CV</a></li>
+                                </ul>
+                            </div>
+                        </td>
+                        </tr>`
 
   });
-}
-
-compilaTabella();
-//card();
-main();
+} main();
